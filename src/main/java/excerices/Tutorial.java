11020195bea93beaba34f91560f577f598c92c83
Theodora2000 <xsimont@stuba.sk>
2021-04-17 11:27:38 +0200

    Java Tutorial 45 (GUI) - Moving Object with Keyboard Inputs (KeyListener)

--- src/main/java/excerices/Tutorial.java
+++ src/main/java/excerices/Tutorial.java
@@ -7,37 +7,82 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Tutorial extends JPanel implements ActionListener{
+public class Tutorial extends JPanel implements ActionListener, KeyListener{
 
     Timer tm = new Timer(5,this);//Gui version of sleep methot, this reference to ActionListener
-    int x=0,velX=2;
-/*
+    int x=0,velX=0, y=0,velY=0;
+
     public Tutorial(){
         tm.start();
-       /* addKeyListener(this);
+        addKeyListener(this);
         setFocusable(true);//enable KeyListener
         setFocusTraversalKeysEnabled(false);//we wontbe using shit or tab key
 
-    }*/
+    }
     public void paint(Graphics g){
         super.paint(g);
         g.setColor(Color.red);
 
-        g.fillRect(x,x,50,30);
-        tm.start();
+        g.fillRect(x,y,50,30);
+
     }
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(x<0 || x > 350){
-            velX = -velX;
+        if(x<0){
+            velX=0;
+            x=0;
+        }
+        if(x>530){
+            velX=0;
+            x=530;
+        }
+        if(y<0){
+            velY=0;
+            y=0;
+        }
+        if(y>330){
+            velY=0;
+            y=330;
         }
         x = x + velX;
-        //y=y+velY;
+        y = y + velY;
         repaint();//repaint rectangle every 5 seconds
     }
 
 
 
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        int c = e.getKeyCode();
+
+        if(c == KeyEvent.VK_LEFT){
+            velX=-1;
+            velY=0;
+        }
+        if(c == KeyEvent.VK_UP){
+            velX=0;
+            velY=-1;
+        }
+        if(c == KeyEvent.VK_RIGHT){
+            velX=1;
+            velY=0;
+        }
+        if(c == KeyEvent.VK_DOWN){
+            velX=0;
+            velY=1;
+        }
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
